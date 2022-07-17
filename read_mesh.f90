1f19f40374fe802fd53fcc6cfcae091c5c03bab9
thawtar <mr.thaw.tar1990@gmail.com>
2022-07-17 16:17:32 +0900

    testing write functionality of fortran

--- read_mesh.f90
+++ read_mesh.f90
@@ -1,3 +1,27 @@
+subroutine output
+	real, dimension(100)::x,y
+	real, dimension(100)::p,q
+	integer::i
+	do i=1,100
+		x(i)=i*0.1
+		y(i)=sin(x(i))*(1-cos(x(i)/3.0))
+	end do
+	
+	! output data into file
+	open (10,file='output.dat',status='new')
+	do i=1,100
+		write(10,*) x(i),y(i)
+	end do
+	close(10)
+end subroutine output
+
+! Write a subroutine to read input files
+subroutine read_input
+	write(*,*)"Read input"
+
+end subroutine read_input
+
+
 program read_mesh
 implicit none
 
@@ -6,8 +30,8 @@ implicit none
 ! 12 = ux.dat
 integer::i,j,k
 write(*,*)"Hello World!"
+call output()
 stop
 end program read_mesh
 
-! Write a subroutine to read input files
-subroutine read_input()
+
